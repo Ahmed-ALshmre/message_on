80e7a8ddfb11e70fab49f12d8cbcb8ed506a71a8
ahmed alshmre <61561582+Ahmed-ALshmre@users.noreply.github.com>
2022-08-31 20:10:16 +0300

    Update firebase_db.dart

--- chat/db/firebase_db.dart
+++ chat/db/firebase_db.dart
@@ -1,7 +1,10 @@
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:messages/controller/functions.dart';
 import 'package:messages/model/addNewUser.dart';
+import 'package:messages/model/notification.dart';
 import 'package:messages/model/user.dart';
 import '../controller/state_management.dart';
 import '../model/chat.dart';
@@ -11,10 +14,10 @@ class FirestoreDb {
   static FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
   static Controller get = Get.put(Controller());
 
-  static addMessage(Message message, String groupChatId) async {
+  static addMessage(MessageModel message, String groupChatId) async {
     print(groupChatId);
     try {
-      var documentReference = FirebaseFirestore.instance
+      var documentReference = firebaseFirestore
           .collection('messages')
           .doc(groupChatId)
           .collection(groupChatId)
@@ -22,14 +25,16 @@ class FirestoreDb {
           .set(message.toJson())
           .then((value) {
         Funct.saveState(message.idTo);
+        NotificationModel notification = NotificationModel(content: message.content, type: message.type.toString(), peerUserName: '', tokePeer: '');
+         Funct.sendNotification(notification);
       });
     } catch (E) {
       showErrorMessage();
     }
   }
 
-  static Stream<List<Message>> messageStream(groupChatId, int lim) {
-    return FirebaseFirestore.instance
+  static Stream<List<MessageModel>> messageStream(groupChatId, int lim) {
+    return firebaseFirestore
         .collection('messages')
         .doc(groupChatId)
         .collection(groupChatId)
@@ -37,10 +42,10 @@ class FirestoreDb {
         .limit(lim)
         .snapshots()
         .map((QuerySnapshot query) {
-      List<Message> messages = [];
+      List<MessageModel> messages = [];
       for (var todo in query.docs) {
         final messageModel =
-            Message.fromDocumentSnapshot(documentSnapshot: todo);
+            MessageModel.fromDocumentSnapshot(documentSnapshot: todo);
         messages.add(messageModel);
       }
       return messages;
@@ -60,7 +65,7 @@ class FirestoreDb {
   static void updateShow(String id, idTo, groupChar, groupChatId) {
     try {
       if (idTo == get.userId()) {
-        FirebaseFirestore.instance
+        firebaseFirestore
             .collection('messages')
             .doc(groupChar)
             .collection(groupChatId)
@@ -77,31 +82,36 @@ class FirestoreDb {
   static Future<String> initChatScreen(UserChat userChat) async {
     try {
       String id = "";
-      final checkUser =
-          await firebaseFirestore.collection('user').doc(userChat.id).get();
-      if (checkUser.exists) {
-        id = userChat.id;
-      } else {
-        final req = await firebaseFirestore
-            .collection('user')
-            .doc(userChat.id)
-            .set(userChat.toJson())
-            .then((_) {
+      if (userChat != null) {
+        final checkUser =
+            await firebaseFirestore.collection('user').doc(userChat.id).get();
+        if (checkUser.exists) {
           id = userChat.id;
-          get.userId(userChat.id);
-        }).catchError((e) {
-          print(e);
-        });
-      }
+        } else {
+          final req = await firebaseFirestore
+              .collection('user')
+              .doc(userChat.id)
+              .set(userChat.toJson())
+              .then((_) {
+            id = userChat.id;
+            get.userId(userChat.id);
+          }).catchError((e) {
+            print(e);
+          });
+        }
+      } else {}
       return id;
     } catch (E) {
+      showErrorMessage();
       return '';
     }
   }
+
   static void updateNewMessages(peerId) {
+    print('this is peer id $peerId');
     Controller _getCon = Get.put(Controller());
     try {
-      FirebaseFirestore.instance
+      firebaseFirestore
           .collection('user')
           .doc(_getCon.userId())
           .collection('chat')
@@ -109,9 +119,32 @@ class FirestoreDb {
           .update({
         'is_new_message': false,
       });
-    } catch (r) {}
+    } catch (r) {
+
+    }
   }
-  static Future<UserChat>addUserToChat(NewUserAddToChatModel newUserAddToChatModel) async {
+
+  static Future<UserChat?> checkUserExists(String id,
+      NewUserAddToChatModel newUserAddToChatModel) async {
+    final user = await firebaseFirestore
+        .collection('user')
+        .doc(id)
+        .collection('chat')
+        .doc(newUserAddToChatModel.id)
+        .get();
+    if (user.exists) {
+      print(user.data().toString());
+      UserChat userChat =
+          UserChat.fromJson(user.data() as Map<String, dynamic>);
+      return userChat;
+    } else {
+      UserChat? userChat = await addUserToChat(newUserAddToChatModel);
+      return userChat ?? null;
+    }
+  }
+
+  static Future<UserChat?> addUserToChat(
+      NewUserAddToChatModel newUserAddToChatModel) async {
     try {
       Controller _getx = Get.put(Controller());
       final peer = await FirebaseFirestore.instance
@@ -122,6 +155,7 @@ class FirestoreDb {
           .collection('user')
           .doc(_getx.userId.value)
           .get();
+
       if (user.exists && peer.exists) {
         FirebaseFirestore.instance
             .collection('user')
@@ -149,41 +183,42 @@ class FirestoreDb {
               .collection('chat')
               .doc(_getx.userId.value)
               .set({
-            'id': peer['id'],
-            'token': peer['token'],
+            'id': user['id'],
+            'token': user['token'],
             'block_by': '',
             'show_bot': false,
             'is_blocked': false,
-            'is_provider': peer['is_provider'],
+            'is_provider': user['is_provider'],
             'is_new_message': false,
             'mute': false,
-            'name': peer['name'],
-            'photoUrl': peer['photoUrl'],
+            'name': user['name'],
+            'photoUrl': user['photoUrl'],
             'idProduct': newUserAddToChatModel.idProduct,
             'imageProduct': newUserAddToChatModel.imageProduct,
             'titleProduct': newUserAddToChatModel.titleProduct,
           });
         });
       } else {
-        return UserChat.fromJson({});
+        return null;
       }
-      return UserChat.fromJson({ 'id': user['id'],
-        'token': user['token'],
+      return UserChat.fromJson({
+        'id': peer['id'],
+        'token': peer['token'],
         'block_by': '',
         'show_bot': false,
         'is_blocked': false,
-        'is_provider': user['is_provider'],
+        'is_provider': peer['is_provider'],
         'is_new_message': false,
         'mute': false,
-        'name': user['name'],
-        'photoUrl': user['photoUrl'],
+        'name': peer['name'],
+        'photoUrl': peer['photoUrl'],
         'idProduct': newUserAddToChatModel.idProduct,
         'imageProduct': newUserAddToChatModel.imageProduct,
-        'titleProduct': newUserAddToChatModel.titleProduct,});
+        'titleProduct': newUserAddToChatModel.titleProduct,
+      });
     } catch (e) {
       showErrorMessage();
-      return UserChat.fromJson({});
-    
+      return null;
     }
   }
 }
