c91b3af54fa3710c2d8cdeb059ae080bd17c572b
ahmed alshmre <61561582+Ahmed-ALshmre@users.noreply.github.com>
2022-08-31 12:42:05 +0300

    Delete lib directory

--- lib/utility/widget.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-
-Widget get loading => const Center(child: CircularProgressIndicator(),);
-
-void showErrorMessage(){
-  Get.showSnackbar(GetSnackBar(message: "لقد حدث خطا تاكد من اتصالك بالشبكة",title: "لقد حدث خطا",duration: Duration(milliseconds: 1000),));
-}
