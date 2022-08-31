0f989fe4c2cab4d8492474cebb9d008d54be174e
ahmed alshmre <61561582+Ahmed-ALshmre@users.noreply.github.com>
2022-08-31 20:08:59 +0300

    Update functions.dart

--- chat/controller/functions.dart
+++ chat/controller/functions.dart
@@ -9,87 +9,11 @@ import 'package:intl/intl.dart';
 import 'package:messages/controller/firebase_contoller.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../model/notification.dart';
 import 'state_management.dart';
 
 class Funct {
   static FirebaseFirestore _firebaseClass = FirebaseFirestore.instance;
-  static Future<void> answer(var data) async {}
-
-  static var ans = {
-    'qus': '',
-    'answer': 'وعليكم الاسلام',
-    'key': '1',
-  };
-
-  static void onSendMessage(String content, int type, groupChatId, id, peerId) {
-    // type: 0 = text, 1 = image, 2 = sticker
-    if (content.trim() != '') {
-      String _time = DateTime.now().millisecondsSinceEpoch.toString();
-      var documentReference = FirebaseFirestore.instance
-          .collection('messages')
-          .doc(groupChatId)
-          .collection(groupChatId)
-          .doc(_time);
-      FirebaseFirestore.instance.runTransaction((transaction) async {
-        transaction.set(
-          documentReference,
-          {
-            'idFrom': peerId,
-            'isShow': false,
-            'idTo': id,
-            'timestamp': _time,
-            'content': content,
-            'type': type,
-          },
-        );
-      }).then((value) async {
-        /*
-        if (!controller.isOffline()) {
-          print("this is type =>$type");
-          String getToken = await Funct.getTokenPaer(peerId);
-          await Funct.sendNotifOneSgn(" لقد ارسل: ${controller.nameUser.value.toString()}", [getToken], "${type == 0 ?content:""}",{'type':'1','uid':controller.userId.value.toString()},type == 1 ?  content:'');
-        } else {}
-        Funct.updateList(controller.userIndexChat(), peerId);
-       */
-      });
-    } else {}
-  }
-
-  static void onSendMessage1(
-      String content, int type, groupChatId, id, peerId) {
-    // type: 0 = text, 1 = image, 2 = sticker
-    if (content.trim() != '') {
-      String _time = DateTime.now().millisecondsSinceEpoch.toString();
-      var documentReference = FirebaseFirestore.instance
-          .collection('messages')
-          .doc(groupChatId)
-          .collection(groupChatId)
-          .doc(_time);
-      FirebaseFirestore.instance.runTransaction((transaction) async {
-        transaction.set(
-          documentReference,
-          {
-            'idFrom': peerId,
-            'isShow': false,
-            'idTo': id,
-            'timestamp': _time,
-            'content': content,
-            'type': type,
-          },
-        );
-      }).then((value) async {
-        /*
-        if (!controller.isOffline()) {
-          print("this is type =>$type");
-          String getToken = await Funct.getTokenPaer(peerId);
-          await Funct.sendNotifOneSgn(" لقد ارسل: ${controller.nameUser.value.toString()}", [getToken], "${type == 0 ?content:""}",{'type':'1','uid':controller.userId.value.toString()},type == 1 ?  content:'');
-        } else {}
-        Funct.updateList(controller.userIndexChat(), peerId);
-       */
-      });
-    } else {}
-  }
-
   static Future<File> getFile(ImageSource source) async {
     File file;
     final ImagePicker _picker = ImagePicker();
@@ -159,31 +83,12 @@ class Funct {
     }).then((value) {});
   }
 
-  static Future<void> setStatus(String status, id) async {
-    Controller controller = Get.put(Controller());
-    await FirebaseFirestore.instance.collection('user').doc(id).update({
-      "status": status,
-    });
-    controller.status(status == "Online" ? true : false);
-  }
-
   static String convertTimerToDataTime(String date) {
     int _date = int.parse(date);
     var dt = DateTime.fromMillisecondsSinceEpoch(_date);
     var timeH = DateFormat('HH:mm').format(dt);
 
     return timeH.toString();
-  }
-
-  static List<String> getListId() {
-    MessageController messageController = Get.put(MessageController());
-    List<String> idMessage = [];
-    for (int i = 0; i < messageController.messages.length; i++) {
-      print(messageController.messages[i].content);
-      String idSing = messageController.messages[i].time;
-      idMessage.add(idSing);
-    }
-    return idMessage;
   }
 
   static Future<void> saveState(String id) async {
@@ -198,23 +103,7 @@ class Funct {
     }
   }
 
-  Widget timeAndSee(String time, bool isShow) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        isShow
-            ? Image.asset(
-          'assets/icon/check_double.png',
-          height: 15,
-          width: 15,
-          fit: BoxFit.cover,
-        )
-            : Icon(
-          CupertinoIcons.check_mark,
-          size: 15,
-        ),
-      ],
-    );
+  static void sendNotification(NotificationModel notification)  {
+    // send notification
   }
 }
